a21c88b51c93c03cc2f84caf750c6395a077c9f6
Geoff Kimball <gakimball@bsu.edu>
2014-11-12 00:04:57 -0800

    Add separate file for commands, add help and version commands

--- cli.js
+++ cli.js
@@ -1,46 +1,35 @@
 #!/usr/bin/env node
 
-var fs     = require('fs');
-var exec   = require('exec');
-var colors = require('colors');
-var git    = require('nodegit');
+var nopt       = require('nopt');
+var foundation = require('./lib/foundationCLI');
 
-var args = process.argv.slice(2);
+var options = {
+  "libsass": Boolean,
+  "version": String
+}
+var shorthands = {
+  "v": "version"
+}
+var parsed = nopt(options);
+var cmd = {
+  args: parsed.argv.remain,
+  opts: parsed
+}
 
 // No other arguments given
-if (typeof args[0] === 'undefined') {
-  console.error("You didn't type nothing in.")
+if (typeof cmd.args[0] === 'undefined') {
+  if (typeof cmd.opts.version !== 'undefined') {
+    process.stdout.write("Foundation CLI version " + require('./package.json').version + '\n');
+  }
+  else {
+    foundation.help([]);
+  }
 }
 else {
-  // git.Repo.clone('https://github.com/zurb/foundation-apps.git', "./"+args[0], null, function(err, repo) {
-  //   if (err) {
-  //     console.log(err);
-  //   }
-  //   else {
-  //     console.log("It worked!");
-  //   }
-  // });
-
-  var gitClone = ['git', 'clone', 'https://github.com/zurb/foundation-apps.git', args[0]];
-  var npmInstall = ['npm', 'install'];
-
-  process.stdout.write("Downloading the Foundation for Apps stack...".cyan);
-  exec(gitClone, function(err, out, code) {
-    if (err instanceof Error) throw err;
-    else {
-      process.stdout.write(" Done downloading!\n");
-      process.stdout.write("\nInstalling dependencies...".cyan);
-      exec(npmInstall, {cwd: './'+args[0]}, function(err, out, code) {
-        if (err instanceof Error) throw err;
-        else {
-          process.stdout.write(" Done installing!\n");
-          process.stdout.write("\nYou're all set! Now run ");
-          process.stdout.write("npm test ".cyan);
-          process.stdout.write("while inside the ");
-          process.stdout.write(args[0].cyan)
-          process.stdout.write(" folder.\n\n");
-        }
-      })
-    }
-  });
+  if (typeof foundation[cmd.args[0]] == 'undefined') {
+    console.error("That ain't a command.");
+  }
+  else {
+    foundation[cmd.args[0]](cmd.args.slice(1), cmd.opts);
+  }
 }
