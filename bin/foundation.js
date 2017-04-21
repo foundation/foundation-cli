37cad946dfe2e00d5897827e58ec1c1e75400e4f
Chris <chrisdavidramos@gmail.com>
2017-04-21 18:37:40 -0400

    sorry but lots brb

--- bin/foundation.js
+++ bin/foundation.js
@@ -5,10 +5,11 @@ var update     = require('update-notifier');
 var pkg        = require('../package.json');
 var foundation = require('../lib');
 
+
 // Options that can be passed to commands
 var options = {
     "version": Boolean,
-    "framework": ['sites','apps','emails'],
+    "framework": [String,'sites','apps','emails'],
     "template": String,
     "directory": String
 }
@@ -29,8 +30,6 @@ var cmd = {
     opts: parsed
 }
 
-
-
 // Check for updates once a day
 var notifier = update({
     packageName: pkg.name,
@@ -38,11 +37,12 @@ var notifier = update({
 });
 notifier.notify();
 
+
 // No commands issued.
 if (cmd.command.length === 0) {
     // If -v or --version was passed, show the version of the CLI
     if (cmd.opts.version) {
-        process.stdout.write("Foundation CLI version " + require('../package.json').version + '\n');
+        foundation.converse("Foundation CLI version " + require('../package.json').version);
     }
     // Otherwise, just show the help screen
     else {
@@ -52,10 +52,11 @@ if (cmd.command.length === 0) {
 
 // Commands given
 else {
+
     // If the command typed in doesn't exist, show the help screen
     if (!(cmd.command[0] in foundation)) {
-        console.log(cmd.command[0].red + " is not a defined command.");
-        foundation.help();
+        foundation.converse(cmd.command[0].red + " is not a defined command.");
+       foundation.help(false);
     }
     // Otherwise, just run it already!
     else {
